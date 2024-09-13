0eab366dedbf2e3e10a3706cb96bb3475bdae2ee
Simran Toor <simrankaurtoor@outlook.com>
2024-09-13 17:16:17 +0100

    012_add_one completed

---
+++ 012_add_one.py
@@ -0,0 +1,15 @@
+# more useful function 
+
+def add_one(num):
+    return num + 1
+
+# how it's used
+
+add_one(6)
+
+# print it
+
+print("add_one(6) returns:")
+print(add_one(6))
+
+# `add_one` is a function that takes a number as an input parameter, adds one to it, and then retunr the result
