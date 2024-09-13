59157b5d18df5058e596f76ceb51cd7707dd5406
Simran Toor <simrankaurtoor@outlook.com>
2024-09-13 17:12:38 +0100

    011_identity completed

---
+++ 011_identity.py
@@ -0,0 +1,17 @@
+def just_return_it(num):
+    return num
+
+# this is a function (reusable block of code)
+# name: `just_return_it`
+# parameter: `num` - input/argument
+# body: `return num` - processing input and returns an output
+
+# call a function:
+
+just_return_it(4)
+# returns 4
+
+# use print to see and return it
+
+print("just_return_it(4) returns:")
+print(just_return_it(4))
