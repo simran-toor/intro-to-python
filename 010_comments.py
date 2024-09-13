1b0a2700ce7c79be7bbd4cc53cc71ecf921143bf
Simran Toor <simrankaurtoor@outlook.com>
2024-09-13 17:05:19 +0100

    010_comments completed

---
+++ 010_comments.py
@@ -0,0 +1,11 @@
+# going to be learning the fundamentals of python:
+# functions 
+# arithmetic
+# expressions and statements
+# stings
+# conditionals
+# lists
+# loops
+# dictionaries (pairs of items)
+
+# lines starting with '#' are comments and not run by python 
