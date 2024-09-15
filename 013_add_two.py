6d78357460b3b47b56a9176ec115bf2a7f05293b
Simran Toor <simrankaurtoor@outlook.com>
2024-09-15 19:25:44 +0100

    013_add_two completed

---
+++ 013_add_two.py
@@ -0,0 +1,21 @@
+# @TASK: Write a function called `add_two` that:
+#
+# * Takes a number as input
+# * Adds two to it
+# * Returns the result
+
+# YOUR FUNCTION GOES BELOW THIS LINE
+
+def add_two(num):
+    return num + 2
+
+# YOUR FUNCTION GOES ABOVE THIS LINE
+
+# @TASK: To check your work, run this in the shell:
+
+#   python 013_add_two.py
+
+# This will run the test at the bottom of this file.
+
+print("add_two(6) returns:")
+print(add_two(6))
