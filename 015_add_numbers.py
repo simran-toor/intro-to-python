278155c77678741c22a010b07b4d17c822cc890e
Simran Toor <simrankaurtoor@outlook.com>
2024-09-15 19:58:56 +0100

    015_add_numbers completed

---
+++ 015_add_numbers.py
@@ -0,0 +1,24 @@
+# @TASK: Now you try. Here's an exercise for you:
+#
+# Write a function called `add_numbers` that:
+#
+# * Takes two numbers as input
+# * Adds them together
+# * Returns the result
+
+# YOUR FUNCTION GOES BELOW THIS LINE
+
+def add_numbers(num_a, num_b):
+    return num_a + num_b
+
+print("add_numbers(2, 3) is:")
+print(add_numbers(2, 3))
+
+print("add_numbers(3, 5) is:")
+print(add_numbers(3, 5))
+# YOUR FUNCTION GOES ABOVE THIS LINE
+
+# @TASK: Check your work by running:
+
+#   python 015_add_numbers.py
+
