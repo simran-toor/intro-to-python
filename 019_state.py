08a022978dc4e6964f830ac78073d7290fb8db9f
Simran Toor <simrankaurtoor@outlook.com>
2024-09-16 16:35:54 +0100

    019_state completed

---
+++ 019_state.py
@@ -0,0 +1,14 @@
+# Here's a mindbender for you:
+
+a = 10
+b = 20
+a = b
+print(f"a is {a}")
+print(f"b is {b}")
+
+# @TASK: What does that output? And why? Take a guess, then
+# run the code and see.
+# expect a to equal 20 because `a = b` is changing the state of a to the state of b
+
+# Was it what you expected?
+# yes
